c7a4cae3b33c4a1cf04cfc4a7378ee294e71b8d9
DevnityCode <devnitycode@gmail.com>
2025-06-25 17:03:19 +0300

    Contact.jsx

--- src/conponents/contact/Contact.jsx
+++ src/conponents/contact/Contact.jsx
@@ -4,6 +4,7 @@ import { useForm, ValidationError } from "@formspree/react";
 import Lottie from "lottie-react";
 import doneAnimation from "../animation/done.json";
 import emailAnimation from "../animation/email.json";
+
 const Contact = () => {
   const [state, handleSubmit, reset] = useForm("xwpbnzke");
   const [email, setEmail] = useState("");
@@ -40,10 +41,16 @@ const Contact = () => {
         Contact us
       </h1>
       <p className="sub-title">
-        Contact Us for more information and Get notified when I publish
-        something new.
+        Contact Us for more information and Get notified when I publish something new.
       </p>
-      <div style={{ justifyContent: "space-between" }} className="flex">
+      <div className="animation animation-top">
+        <Lottie
+          className="contact-animation"
+          style={{ height: 300 }}
+          animationData={emailAnimation}
+        />
+      </div>
+      <div className="flex" style={{ justifyContent: "space-between" }}>
         <form onSubmit={handleSubmit}>
           <div className="flex">
             <label htmlFor="email">Email Address</label>
@@ -56,11 +63,7 @@ const Contact = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={state.errors}
-            />
+            <ValidationError prefix="Email" field="email" errors={state.errors} />
           </div>
 
           <div className="flex" style={{ marginTop: "24px" }}>
@@ -72,11 +75,7 @@ const Contact = () => {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             ></textarea>
-            <ValidationError
-              prefix="Message"
-              field="message"
-              errors={state.errors}
-            />
+            <ValidationError prefix="Message" field="message" errors={state.errors} />
           </div>
 
           <button className="submit" disabled={state.submitting}>
@@ -85,21 +84,16 @@ const Contact = () => {
 
           {showSuccessMessage && (
             <p
-              className="flex"
+              className="flex align-center"
               style={{ fontSize: "18px", marginTop: "1.7rem", color: "white" }}
             >
-              <Lottie
-                loop={false}
-                style={{ height: 37 }}
-                animationData={doneAnimation}
-              />
+              <Lottie loop={false} style={{ height: 37 }} animationData={doneAnimation} />
               Your message has been sent successfully.
             </p>
           )}
         </form>
 
-        <div className="animation ">
-          {" "}
+        <div className="animation animation-side">
           <Lottie
             className="contact-animation"
             style={{ height: 400 }}
